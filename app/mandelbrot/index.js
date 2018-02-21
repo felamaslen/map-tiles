d1cc5d158085547ca88e854352e2dfe9cf8bd52a
Fela Maslen <felamaslen@gmail.com>
2018-02-21 23:17:19 +0000

    Use C program for image generation

--- app/mandelbrot/index.js
+++ app/mandelbrot/index.js
@@ -1,75 +1,8 @@
-const { PNG } = require('pngjs');
+const { spawn } = require('child_process');
+const path = require('path');
 
-const MAX_ITERATIONS = 1000;
-const RESOLUTION = 512;
-const TILE_SIZE_PIXELS = 512;
 const MAP_RANGE = [-2, 1, -1, 1];
 const MAX_ZOOM = 10;
-
-function recursiveMandelbrotSetTest(valX, valY, zTestX = 0, zTestY = 0, iteration = 0) {
-    // test if Z = X + iY is in the mandelbrot set
-    // note that this results in an approximation (i.e. high-likelihood guess)
-
-    const zNextX = zTestX ** 2 - zTestY ** 2 + valX;
-    const zNextY = 2 * zTestX * zTestY + valY;
-
-    const zNextAbsoluteValue = zNextX ** 2 + zNextY ** 2;
-
-    if (zNextAbsoluteValue <= 4) {
-        if (iteration < MAX_ITERATIONS) {
-            return recursiveMandelbrotSetTest(valX, valY, zNextX, zNextY, iteration + 1);
-        }
-
-        return true;
-    }
-
-    return false;
-}
-
-function generateTile(minX, maxY, tileSize) {
-    const resolutionFactor = TILE_SIZE_PIXELS / RESOLUTION;
-
-    const png = new PNG({
-        width: TILE_SIZE_PIXELS,
-        height: TILE_SIZE_PIXELS,
-        filterType: -1
-    });
-
-    // using for loops for performance
-
-    for (let iY = 0; iY < RESOLUTION; iY++) {
-
-        const pixYStart = Math.floor(iY * resolutionFactor);
-        const pixYEnd = Math.floor((iY + 1) * resolutionFactor);
-
-        const valY = maxY - iY * tileSize / RESOLUTION;
-
-        for (let iX = 0; iX < RESOLUTION; iX++) {
-
-            const pixXStart = Math.floor(iX * resolutionFactor);
-            const pixXEnd = Math.floor((iX + 1) * resolutionFactor);
-
-            const valX = minX + tileSize * iX / RESOLUTION;
-
-            const valInSet = recursiveMandelbrotSetTest(valX, valY);
-            const color = 0xff * (!valInSet >> 0);
-
-            for (let pixY = pixYStart; pixY < pixYEnd; pixY++) {
-                for (let pixX = pixXStart; pixX < pixXEnd; pixX++) {
-
-                    const index = (png.width * pixY + pixX) << 2;
-
-                    png.data[index] = color;
-                    png.data[index + 1] = color;
-                    png.data[index + 2] = color;
-                    png.data[index + 3] = 0xff;
-                }
-            }
-        }
-    }
-
-    return png.pack();
-}
 
 function getRangeFromTile(tileX, tileY, zoom) {
     const maxTileSize = Math.min(MAP_RANGE[3] - MAP_RANGE[2], MAP_RANGE[1] - MAP_RANGE[0]);
@@ -83,6 +16,12 @@ function getRangeFromTile(tileX, tileY, zoom) {
     return { minX, maxY, tileSize };
 }
 
+function generateTile(minX, maxY, tileSize) {
+    const child = spawn(path.join(__dirname, '../../lib/mandelbrot/mandelbrot'), [minX, maxY, tileSize]);
+
+    return child.stdout;
+}
+
 function routeMandelbrot() {
     return (req, res) => {
         const tileX = Math.round(Number(req.params.posX) || 0);
@@ -90,8 +29,6 @@ function routeMandelbrot() {
         const zoom = Math.min(MAX_ZOOM, Math.max(0, Math.floor(Number(req.params.zoom) || 0)));
 
         const { minX, maxY, tileSize } = getRangeFromTile(tileX, tileY, zoom);
-
-        console.log(minX.toFixed(2), maxY.toFixed(2), tileSize.toFixed(2));
 
         const png = generateTile(minX, maxY, tileSize);
 
